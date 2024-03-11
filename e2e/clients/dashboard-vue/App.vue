994c5764f5c8214c297d6e58daf0428568393ef7
JasminRuddor <jasminruddorjasminruddorjasmin@gmail.com>
2024-03-11 08:11:01 +0300

    Refactor for better maintainability_e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :batooty="batooty" />
+</template>
+
+<script>
+import Vue from 'vue'
+import batooty from '@batooty/core'
+import { Dashboard } from '@batooty/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    batooty: () => new batooty(),
+  },
+}
+</script>
+
+<style src="@batooty/core/dist/style.css"></style>
+<style src="@batooty/dashboard/dist/style.css"></style>
