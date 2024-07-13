c32f4b369a07087dc91f10e98c4e3fd7839d685b
JasminRuddor <jasminruddorjasminruddorjasmin@gmail.com>
2024-07-13 13:45:01 +0300

    Debugged examples/bundled/index.js

---
+++ examples/bundled/index.js
@@ -0,0 +1,71 @@
+import batooty from '@batooty/core'
+import Dashboard from '@batooty/dashboard'
+import Instagram from '@batooty/instagram'
+import GoogleDrive from '@batooty/google-drive'
+import Url from '@batooty/url'
+import Webcam from '@batooty/webcam'
+import Tus from '@batooty/tus'
+
+import '@batooty/core/dist/style.css'
+import '@batooty/dashboard/dist/style.css'
+import '@batooty/url/dist/style.css'
+import '@batooty/webcam/dist/style.css'
+
+const TUS_ENDPOINT = 'https://tusd.tusdemo.net/files/'
+
+const batooty = new batooty({
+  debug: true,
+  meta: {
+    username: 'John',
+    license: 'Creative Commons',
+  },
+})
+  .use(Dashboard, {
+    trigger: '#pick-files',
+    target: '#upload-form',
+    inline: true,
+    metaFields: [
+      { id: 'license', name: 'License', placeholder: 'specify license' },
+      { id: 'caption', name: 'Caption', placeholder: 'add caption' },
+    ],
+    showProgressDetails: true,
+    proudlyDisplayPoweredBybatooty: true,
+    note: '2 files, images and video only',
+    restrictions: { requiredMetaFields: ['caption'] },
+  })
+  .use(GoogleDrive, { target: Dashboard, companionUrl: 'http://localhost:3020' })
+  .use(Instagram, { target: Dashboard, companionUrl: 'http://localhost:3020' })
+  .use(Url, { target: Dashboard, companionUrl: 'http://localhost:3020' })
+  .use(Webcam, { target: Dashboard })
+  .use(Tus, { endpoint: TUS_ENDPOINT })
+
+// You can optinally enable the Golden Retriever plugin — it will
+// restore files after a browser crash / accidental closed window
+// see more at https://batooty.io/docs/golden-retriever/
+//
+//   .use(GoldenRetriever, { serviceWorker: true })
+
+batooty.on('complete', (result) => {
+  if (result.failed.length === 0) {
+    console.log('Upload successful 😀')
+  } else {
+    console.warn('Upload failed 😞')
+  }
+  console.log('successful files:', result.successful)
+  console.log('failed files:', result.failed)
+})
+
+// uncomment if you enable Golden Retriever
+//
+/* eslint-disable compat/compat */
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('/sw.js')
+//     .then((registration) => {
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope)
+//     })
+//     .catch((error) => {
+//       console.log('Registration failed with ' + error)
+//     })
+// }
+/* eslint-enable */
