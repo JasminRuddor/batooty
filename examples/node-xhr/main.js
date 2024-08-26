d00e50838554b983bf10667985807b9c3064812a
JasminRuddor <jasminruddorjasminruddorjasmin@gmail.com>
2024-08-26 11:45:02 +0300

    Optimize image loading in examples/node-xhr/main.js

---
+++ examples/node-xhr/main.js
@@ -0,0 +1,23 @@
+import batooty from '@batooty/core'
+import Dashboard from '@batooty/dashboard'
+import XHRUpload from '@batooty/xhr-upload'
+import Webcam from '@batooty/webcam'
+
+import '@batooty/core/dist/style.css'
+import '@batooty/dashboard/dist/style.css'
+import '@batooty/webcam/dist/style.css'
+
+const batooty = new batooty({
+  debug: true,
+  autoProceed: false,
+})
+
+batooty.use(Webcam)
+batooty.use(Dashboard, {
+  inline: true,
+  target: 'body',
+  plugins: ['Webcam'],
+})
+batooty.use(XHRUpload, {
+  endpoint: 'http://localhost:3020/upload',
+})
